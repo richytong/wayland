8cc087b36c6da70fbd28b985fe8861d1f933553e
Richard <richytong@gmail.com>
2019-11-03 01:33:46 -0800

    1

---
+++ index.js
@@ -0,0 +1,34 @@
+const _ = require('fnal')
+const grpc = require('grpc')
+const protoLoader = require('@grpc/proto-loader')
+const uuidv4 = require('uuid/v4')
+
+const loadProto = _.syncFlow(protoLoader.loadSync, grpc.loadPackageDefinition)
+
+const makeServer = proto => services => {
+  const server = new grpc.Server()
+  for (const [k, v] of Object.entries(services)) {
+    server.addService(proto[k].service, v)
+  }
+  return server
+}
+
+module.exports = _.syncFlow(
+  x => ({ ...x, proto: loadProto(x.protopath) }),
+  x => ({ ...x, server: makeServer(x.proto)(x.services) }),
+  x => ({ ...x,
+    start: (y = {}) => {
+      const name = y.name || x.name || 'Server'
+      const host = y.host || x.host || 'localhost'
+      const port = y.port || x.port || 3000
+      const credentials = x.credentials || y.credentials || (
+        grpc.ServerCredentials.createInsecure()
+      )
+      (y.hello || x.hello || (() => {
+        console.log(`${name} running at ${host}:${port}`)
+      }))()
+      x.server.bind(`${host}:${port}`, credentials)
+      x.server.start()
+    },
+  }),
+)
